8db988fefc48a88e3e467027e54ffc4335e037d0
Nati Namvong <nati@flyingcomma.com>
2016-09-10 15:41:11 +0700

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,53 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+    <app-header></app-header>
+    <menus :menus="menus" :remove-menu="removeMenu"></menus>
+    <new-menu :input-menu="inputMenu" :add-menu="addMenu" :on-input="onInput"></new-menu>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import AppHeader from './components/AppHeader'
+import Menus from './components/Menus'
+import NewMenu from './components/NewMenu'
 
 export default {
   components: {
-    Hello
+    AppHeader,
+    Menus,
+    NewMenu
+  },
+  data () {
+    return {
+      inputMenu: '',
+      menus: [
+        {
+          name: 'สุกี้น้ำ'
+        },
+        {
+          name: 'คั่วกลิ้ง'
+        }
+      ]
+    }
+  },
+  methods: {
+    addMenu (name) {
+      name = name.trim()
+      let isMenuExist = this.menus.find(function (menu) {
+        return menu.name === name
+      })
+      if (name !== '' && !isMenuExist) {
+        let newMenu = { name }
+        this.menus.push(newMenu)
+        this.inputMenu = ''
+      }
+    },
+    removeMenu (index) {
+      this.menus.splice(index, 1)
+    },
+    onInput (e) {
+      this.inputMenu = e.target.value
+    }
   }
 }
 </script>
